cdbf130d3f72aa48e7c16146180fcc1789a0d5cc
songtianyi <songtianyi630@163.com>
2017-04-14 18:20:59 +0800

    test pass

---
+++ demo/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/songtianyi/rrframework/logs"
+	"github.com/songtianyi/wechat-go/plugins/faceplusplus"
+	"github.com/songtianyi/wechat-go/wxweb"
+)
+
+func main() {
+	session, err := wxweb.CreateSession(nil, wxweb.TERMINAL_MODE)
+	if err != nil {
+		logs.Error(err)
+		return
+	}
+	// add plugins
+	faceplusplus.Register(session)
+
+	go func() {
+		for {
+			select {
+			case <-session.RefreshFlag:
+				session, err = wxweb.CreateSession(nil, wxweb.TERMINAL_MODE)
+				if err != nil {
+					logs.Error(err)
+				}
+			}
+		}
+	}()
+	for {
+		if err := session.LoginAndServe(); err != nil {
+			logs.Error("session exit, %s", err)
+		}
+	}
+}

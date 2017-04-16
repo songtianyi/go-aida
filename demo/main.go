a7191d129c6de24fe502b2f54ff7d3a0170fff72
songtianyi <songtianyi630@163.com>
2017-04-16 20:47:07 +0800

    change plugin path

--- demo/main.go
+++ demo/main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"github.com/songtianyi/rrframework/logs"
-	"github.com/songtianyi/wechat-go/plugins/faceplusplus"
+	"github.com/songtianyi/wechat-go/plugins/wxweb/faceplusplus"
+	"github.com/songtianyi/wechat-go/plugins/wxweb/replier"
 	"github.com/songtianyi/wechat-go/wxweb"
 )
 
 func main() {
+	// create session
 	session, err := wxweb.CreateSession(nil, wxweb.TERMINAL_MODE)
 	if err != nil {
 		logs.Error(err)
 		return
 	}
-	// add plugins
+	// add plugins for this session
 	faceplusplus.Register(session)
+	replier.Register(session)
 
+	// watch refresh flag
 	go func() {
 		for {
 			select {
@@ -30,6 +34,7 @@ func main() {
 		}
 	}()
 	for {
+		// watch session
 		if err := session.LoginAndServe(); err != nil {
 			logs.Error("session exit, %s", err)
 		}
